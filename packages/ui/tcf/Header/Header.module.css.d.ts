d29395c2a2f0d3c40f6a1ffe7b6173c69bd6edf9
James Hamilton <james@thechipfactory.co.uk>
2022-05-13 16:18:50 +0900

    add basic tcf Header component

---
+++ packages/ui/tcf/Header/Header.module.css.d.ts
@@ -0,0 +1,7 @@
+declare const styles: {
+  readonly "container": string;
+  readonly "left": string;
+  readonly "right": string;
+};
+export = styles;
+
---
+++ packages/ui/tcf/Header/Header.tsx
@@ -0,0 +1,10 @@
+import * as styles from "./Header.module.css";
+
+const Header = () => (
+    <div className={styles.container}>
+        <div className={styles.left} />
+        <div className={styles.right} />
+    </div>
+)
+
+export default Header;
