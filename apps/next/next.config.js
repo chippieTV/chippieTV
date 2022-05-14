2853a85ef83dcee362b26d40c29fc4cef1cd16f8
James Hamilton <james@thechipfactory.co.uk>
2022-05-15 01:25:08 +0900

    update basePath to .

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -2,5 +2,16 @@
 
 const nextConfig = require('next-transpile-modules')(['ui']); // pass the modules you would like to see transpiled
 nextConfig.reactStrictMode = true;
+nextConfig.basePath = ".";
 
-module.exports = nextConfig
+// nextConfig.webpack = (config, { dev, isServer }) => {
+//     // Replace React with Preact only in client production build
+//     // if (!dev && !isServer) {
+//       Object.assign(config.resolve.alias, {
+//         react: 'preact/compat',
+//         'react-dom/test-utils': 'preact/test-utils',
+//         'react-dom': 'preact/compat',
+//       });
+//     // }
+
+module.exports = nextConfig;
