b3031efd0cf312c0233163b3ae00d269e026e60b
James Hamilton <james@thechipfactory.co.uk>
2022-05-15 20:19:07 +0900

    update nextjs.config.js fixing basePath, assetPrefix and switch to Preact

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -1,21 +1,23 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = require('next-transpile-modules')(['ui']); // pass the modules you would like to see transpiled
-nextConfig.reactStrictMode = true;
+const isProd = process.env.NODE_ENV === "production";
 
-// should fix the files
-// https://wallis.dev/blog/next-js-basepath-and-assetprefix
-nextConfig.basePath = "/chippieTV.github.io";
-nextConfig.assetPrefix = "/chippieTV.github.io/";
+const withTM = require('next-transpile-modules')(['ui']); // pass the modules you would like to see transpiled
 
-// nextConfig.webpack = (config, { dev, isServer }) => {
-//     // Replace React with Preact only in client production build
-//     // if (!dev && !isServer) {
-//       Object.assign(config.resolve.alias, {
-//         react: 'preact/compat',
-//         'react-dom/test-utils': 'preact/test-utils',
-//         'react-dom': 'preact/compat',
-//       });
-//     // }
+module.exports = withTM({
+    basePath: "/chippieTV.github.io",
+    assetPrefix: isProd ? "/chippieTV.github.io/" : "",
+    reactStrictMode: true,
+    webpack: (config, { dev, isServer }) => {
+        // Replace React with Preact only in client production build
+        if (!dev && !isServer) {
+          Object.assign(config.resolve.alias, {
+            react: 'preact/compat',
+            'react-dom/test-utils': 'preact/test-utils',
+            'react-dom': 'preact/compat',
+          });
+        }
 
-module.exports = nextConfig;
+        return config;
+    }
+});
